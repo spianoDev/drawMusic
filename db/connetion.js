4324bddf27ea41b628ddc5fc663c9b8cc98285a1
spiano <lafleurpiano@gmail.com>
2019-10-08 14:23:59 -0500

    setting up for heroku deployment

--- db/connetion.js
+++ db/connetion.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
+// to allow deployment into heroku
+let mongoURI = "";
+if (process.env.NODE_ENV === "production") {
+    mongoURI = process.env.DB_URL;
+} else {
+    mongoURI = "mongodb://localhost/drawMusic";
+}
 
 mongoose.connect('mongodb://localhost/drawMusic', { useNewUrlParser: true }, () => {
     console.log('connecting music in 3, 2, 1...');
