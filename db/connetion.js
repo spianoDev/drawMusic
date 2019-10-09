d5b13554138cbb52356f42c98b01200d30580fb7
spiano <lafleurpiano@gmail.com>
2019-10-09 11:15:19 -0500

    new fix to try

--- db/connetion.js
+++ db/connetion.js
@@ -13,18 +13,21 @@ mongoose.Promise = Promise;
 //     console.log('connecting music in 3, 2, 1...');
 // });
 
-let mongoURI = 'mongodb+srv://sarah:2l2XmOpIBmS4xLCU@drawmusic-nusnj.mongodb.net/drawMusic?retryWrites=true&w=majority';
+let mongoURI = '';
 if (process.env.NODE_ENV === "production") {
     mongoURI = process.env.DB_URL;
 } else {
     mongoURI = "mongodb://localhost/drawMusic";
 }
 
-mongoose.connect(mongoURI, {
+mongoose.connect('mongodb://localhost/drawMusic', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false}, () => {
+    console.log("YAY... connection established!");
+
 })
+
     .catch(error => console.log("connection failed", error));
 
 module.exports = mongoose;
