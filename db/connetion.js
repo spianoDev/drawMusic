f2d9dc36c91b658bef8ca667c2799c9fad30d0ea
spiano <lafleurpiano@gmail.com>
2019-10-09 04:29:27 -0500

    trying a new connection with bluebird to add database to Heroku

--- db/connetion.js
+++ db/connetion.js
@@ -9,8 +9,36 @@ mongoose.Promise = Promise;
 //     mongoURI = "mongodb://localhost/drawMusic";
 // }
 
-mongoose.connect('mongodb://localhost/drawMusic', { useNewUrlParser: true }, () => {
-    console.log('connecting music in 3, 2, 1...');
-});
+// mongoose.connect('mongodb://localhost/drawMusic', { useNewUrlParser: true }, () => {
+//     console.log('connecting music in 3, 2, 1...');
+// });
+
+// From Gabe to try and connect my database to Heroku
+
+global.Promise = require('bluebird');
+
+async function test() {
+    // No unhandled rejection!
+    await Promise.reject(new Error('test'));
+}
+
+// Prints "false"
+console.log(test().catch(() => {}) instanceof require('bluebird'));
+
+
+
+let mongoURI = "";
+if (process.env.NODE_ENV === "production") {
+    mongoURI = process.env.DB_URL;
+} else {
+    mongoURI = "mongodb://localhost/workout";
+}
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+    .catch(error => console.log("connection failed", error));
 
 module.exports = mongoose;
